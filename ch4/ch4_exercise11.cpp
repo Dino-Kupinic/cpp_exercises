8c4884959679af4e3896461952750432d0a5d4b4
Dino Kupinic <dkupinic@htl-steyr.ac.at>
2022-12-10 20:59:22 +0100

    ch4 ex11 & 12 done

---
+++ ch4/ch4_exercise11.cpp
@@ -0,0 +1,33 @@
+//
+// Created by Dino on 10 Dec 2022.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int ch4_exercise11() {
+    vector<int> prime_numbers;
+
+    for (int i = 1; i <= 100; ++i) {
+        bool is_a_prime = true;
+
+        for (int j = 2; j < i; j++) {
+            if (i % j == 0) {
+                is_a_prime = false;
+                break;
+            }
+        }
+
+        if (is_a_prime)
+            prime_numbers.push_back(i);
+    }
+
+    for (int i = 0; i < prime_numbers.size(); ++i) {
+        cout << prime_numbers[i] << " is a prime" << endl;
+    }
+
+    return 0;
+}
+
