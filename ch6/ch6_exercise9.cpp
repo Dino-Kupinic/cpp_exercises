24f18b31ecea6255bb5bf9355bae2c06cce9a1b1
Dino Kupinic <dkupinic@htl-steyr.ac.at>
2022-12-15 19:01:48 +0100

    changed ch6 exer9

--- ch6/ch6_exercise9.cpp
+++ ch6/ch6_exercise9.cpp
@@ -4,21 +4,46 @@
 
 #include <iostream>
 #include <vector>
+#include <regex>
 
 using namespace std;
 
+void display_one_unit(vector<int> vec) {
+    if (vec[0] == 1) {
+        cout << vec[0] << " is " << vec[0] << " one";
+    } else {
+        cout << vec[0] << " is " << vec[0] << " ones";
+    }
+}
 
+// This solution is quite ugly, using a vector makes it really hard to read
+void display_two_units(vector<int> vec) {
+    if (vec[0] > 1 && vec[1] == 1) {
+        cout << vec[0] << vec[1] << " is " << vec[0] << " tens and " << vec[1] << " one";
+    } else if (vec[0] == 1 && vec[1] > 1) {
+        cout << vec[0] << vec[1] << " is " << vec[0] << " ten and " << vec[1] << " ones";
+    } else if (vec[0] == 1 && vec[1] == 1) {
+        cout << vec[0] << vec[1] << " is " << vec[0] << " ten and " << vec[1] << " one";
+    } else {
+        cout << vec[0] << vec[1] << " is " << vec[0] << " tens and " << vec[1] << " ones";
+    }
+}
 
 int ch6_exercise9() {
     vector<char> input_vec;
     vector<int> output_vec;
     char input_digit;
 
+    cout << "Digit Program\nType d to stop input\n";
+
     while (cin >> input_digit) {
         if (input_digit == 'd') {
             break;
+        } else if (isdigit(input_digit)) {
+            input_vec.push_back(input_digit);
+        } else {
+            cout << "Invalid!";
         }
-        input_vec.push_back(input_digit);
     }
 
     for (char i : input_vec) {
@@ -27,12 +52,14 @@ int ch6_exercise9() {
 
     switch (output_vec.size()) {
         case 1:
-            cout << output_vec[0] << " is " << output_vec[0] << " ones";
+            display_one_unit(output_vec);
             break;
         case 2:
-            cout << output_vec[0] << output_vec[1] << " is " << output_vec[1] << " tens and " << output_vec[0] << " ones";
+            display_two_units(output_vec);
             break;
         case 3:
+            // You get the idea with the functions above, so I stopped myself from doing more else if constructs with these
+            // other two cases
             cout << output_vec[0] << output_vec[1] << output_vec[2] << " is " << output_vec[0] << " hundreds and "
             << output_vec[1] << " tens and " << output_vec[2] << " ones";
             break;
